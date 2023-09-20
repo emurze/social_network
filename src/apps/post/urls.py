1d6b1b718e7327d0e35ce5351fbbc9c5f9b9b31e
emurze <rozanov7475@yandex.by>
2023-09-20 22:09:20 +0300

    ends fix posts

--- src/apps/post/urls.py
+++ src/apps/post/urls.py
@@ -4,7 +4,7 @@ from apps.post.pages.posts.views import (
     CreatePost,
     like_post,
     download_replies,
-    create_reply,
+    CreteReply,
     DownloadPosts,
     PostListView,
     FilterPosts,
@@ -15,12 +15,11 @@ app_name = 'post'
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='list'),
-    path('posts/like_post/', like_post, name='like_post'),
-    path('posts/create_reply/', create_reply, name='create_reply'),
-    path('posts/download_replies/', download_replies,
-         name='download_replies'),
-    path('posts/download/', DownloadPosts.as_view(), name='download_posts'),
+    path('posts/like/', like_post, name='like_post'),
     path('posts/create/', CreatePost.as_view(), name='create'),
     path('posts/search/', SearchPosts.as_view(), name='search_posts'),
     path('posts/filter/', FilterPosts.as_view(), name='filter_posts'),
+    path('posts/download/', DownloadPosts.as_view(), name='download_posts'),
+    path('posts/create_reply/', CreteReply.as_view(), name='create_reply'),
+    path('posts/download_replies/', download_replies, name='download_replies'),
 ]
